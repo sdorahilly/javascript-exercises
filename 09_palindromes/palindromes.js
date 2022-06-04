fbf90e32081df05bfa2002f89825f76e903d7255
Sean <sdorahilly@gmail.com>
2022-06-04 14:35:17 -0400

    palindromes completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,28 @@
-const palindromes = function () {
+//First we have to lowercase everything
+//then we have to condense by removing spaces and punctuation
+//then we can reverse the string
+//then compare the reversed string to the current condensed/lowercased string 
+//if equivalent then return true otherwise return false 
 
+const palindromes = function (string) {
+    //lowercases string
+    lowercase = string.toLowerCase();
+    
+    //removes punctuation then removes whitespace 
+    punctuationless = lowercase.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+    condensed = punctuationless.replace(/\s/g, '');
+
+    //reverses string
+    const myArray = Array.from(condensed);
+    let reverse = myArray.reverse();
+    reverse = myArray.join('')
+    
+    //compares reversed string to the orignal input
+    if (reverse === condensed) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
