f9f7880509088606bbda5e03ba994d1ab71395bb
Sean <sdorahilly@gmail.com>
2022-06-04 11:44:47 -0400

    leapYears function completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,25 @@
-const leapYears = function() {
+//just going to be a bunch of if else statements
+//take one number 
+//first is this divisible by 4 --> if not return false
+//then if number is divisible by 100 continue on ---> if not return true
+//if its also divisible by 400 return true if not return false
 
+
+// Note: This can also be done with "&&" but its easier logically for me this way :)
+const leapYears = function(year) {
+    if (year % 4 === 0) {
+        if (year % 100 === 0){
+            if (year % 400 === 0){
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
